ffed5e0ac3ec7834df724b1daf25d9b7f597b8dd
Diego Pino Navarro <dpino@metro.org>
2019-11-18 21:01:35 -0500

    Maybe this will work right away!

--- theme/metrodora-slideshow-widget.tpl.php
+++ theme/metrodora-slideshow-widget.tpl.php
@@ -14,35 +14,24 @@
                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#myCarousel" data-slide-to="1"></li>
                 <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+                <li data-target="#myCarousel" data-slide-to="5"></li>
+
             </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
+              <?php foreach ($objects as $object): ?>
 
                 <div class="item active">
-                    <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+                    <img src="<?php echo $object['iif_url']; ?>" alt="<?php echo $object['object_label']; ?>" style="width:100%;">
                     <div class="carousel-caption">
                         <h3>Los Angeles</h3>
                         <p>LA is always so much fun!</p>
                     </div>
                 </div>
-
-                <div class="item">
-                    <img src="chicago.jpg" alt="Chicago" style="width:100%;">
-                    <div class="carousel-caption">
-                        <h3>Chicago</h3>
-                        <p>Thank you, Chicago!</p>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src="ny.jpg" alt="New York" style="width:100%;">
-                    <div class="carousel-caption">
-                        <h3>New York</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
-                </div>
-
+            <?php endforeach;?>
             </div>
 
             <!-- Left and right controls -->
@@ -56,7 +45,4 @@
             </a>
         </div>
     </div>
-
-
-
 <?php endif;?>
