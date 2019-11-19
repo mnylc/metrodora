012509fe00718019079f32cbc8b7e0ffbbca2520
Diego Pino Navarro <dpino@metro.org>
2019-11-18 21:26:50 -0500

    Updates on carousel

--- theme/metrodora-slideshow-widget.tpl.php
+++ theme/metrodora-slideshow-widget.tpl.php
@@ -17,9 +17,11 @@
                 <li data-target="#FrontPageCarousel" data-slide-to="3"></li>
                 <li data-target="#FrontPageCarousel" data-slide-to="4"></li>
                 <li data-target="#FrontPageCarousel" data-slide-to="5"></li>
-
+                <li data-target="#FrontPageCarousel" data-slide-to="6"></li>
+                <li data-target="#FrontPageCarousel" data-slide-to="7"></li>
+                <li data-target="#FrontPageCarousel" data-slide-to="8"></li>
+                <li data-target="#FrontPageCarousel" data-slide-to="9"></li>
             </ol>
-
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
               <?php foreach ($objects as $object): ?>
@@ -27,19 +29,19 @@
                 <div class="item active">
                     <img src="<?php echo $object['iiif_url']; ?>" alt="<?php echo $object['object_label']; ?>" style="width:100%;">
                     <div class="carousel-caption">
-                        <h3>Los Angeles</h3>
-                        <p>LA is always so much fun!</p>
+                        <h3>NPL</h3>
+                        <p><?php echo $object['object_label']; ?></p>
                     </div>
                 </div>
             <?php endforeach;?>
             </div>
 
             <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <a class="left carousel-control" href="#FrontPageCarousel" data-slide="prev">
                 <span class="glyphicon glyphicon-chevron-left"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <a class="right carousel-control" href="#FrontPageCarousel" data-slide="next">
                 <span class="glyphicon glyphicon-chevron-right"></span>
                 <span class="sr-only">Next</span>
             </a>
